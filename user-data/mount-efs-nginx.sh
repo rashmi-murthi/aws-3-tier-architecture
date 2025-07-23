a5237e3e8b0867cc113dd3ca6286a941b22c5b09
rashmi-murthi <rashmi.pmurthi20@gmail.com>
2025-07-23 22:40:10 +0530

    Update mount-efs-nginx.sh
    Updated few changes

--- user-data/mount-efs-nginx.sh
+++ user-data/mount-efs-nginx.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+# Redirect all outputs (stdout and stderr) to log file
+exec > /var/log/user-data.log 2>&1
 set -e
+set -x
 
 # 1. Update and install required packages
-apt update -y
-apt install -y nginx nfs-common
+apt update -y && apt upgrade -y 
+apt-get install -y nginx nfs-common mysql-client
 
 # 2. Remove default NGINX config
 unlink /etc/nginx/sites-enabled/default || true
@@ -31,7 +35,7 @@ ln -s /etc/nginx/sites-available/eleti.site /etc/nginx/sites-enabled/
 mkdir -p /var/www/eleti.site
 mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-0b08ef45d69a4e3e7.efs.us-east-1.amazonaws.com:/ /var/www/eleti.site
 
-# 5. Start and enable NGINX
-systemctl restart nginx
+# 5. Enable and start NGINX
 systemctl enable nginx
+systemctl restart nginx
 
